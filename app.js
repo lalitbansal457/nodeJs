d9bd6210363c4c5261171857c1ef7e16a923f5e9
Lalit <lalit@SQMLaptop33.local>
2021-10-19 12:25:07 +0530

    Mananging routes

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-	res.setHeader("content-type", "text/html");
+	const url = req.url;
+	console.log(url)
+
+	if(url == "/") {
+		res.setHeader("content-type", "text/html");
+		res.write("<html>")
+		res.write("<head><title>My first page</title></head>")
+		res.write("<body><form action='/post' method='POST'><input type='text' name='name' /><button type='submit'>Submit</button></form></body>")
+		res.write("</html>")
+		return res.end();
+	}
+
+	if(url == "/post" && req.method == "POST" ) {
+		fs.writeFileSync("data.text", "Dummy");
+		res.statusCode = 302;
+		res.setHeader("Location", "/");
+		return res.end();
+	}
+ 	res.setHeader("content-type", "text/html");
 	res.write("<html>")
 	res.write("<head><title>My first page</title></head>")
 	res.write("<body><h1> Welcome geeks</h1></body>")
