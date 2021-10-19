ce0bf7b5d06f8046bd1f771d4e5f7e80a851677d
Lalit <lalit@SQMLaptop33.local>
2021-10-19 11:22:57 +0530

    Seeting up headers and response.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-	
+	res.setHeader("content-type", "text/html");
+	res.write("<html>")
+	res.write("<head><title>My first page</title></head>")
+	res.write("<body><h1> Welcome geeks</h1></body>")
+	res.write("</html>")
+	res.end();
 })
 
 server.listen(3000)
