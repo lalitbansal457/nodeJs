9c34e29b3a2895b02611737e50a5ab5d13ca771c
Lalit <lalit@SQMLaptop33.local>
2021-10-20 08:48:27 +0530

    Added views file.

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const http = require("http");
 const express = require("express");
+const bodyParser = require("express");
+const path = require("path");
 //const route = require("./route");
+const adminRoute = require("./routes/admin");
+const shopRoute = require("./routes/shop");
 
 const app = express();
+app.use('/admin', adminRoute);
+app.use(shopRoute);
 
 app.use((req, res, next) => {
-	console.log("Inside request")
-	next();
+	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(3000)
