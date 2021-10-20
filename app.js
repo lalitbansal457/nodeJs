b70d35479958133813dd6b7aadbb5883b82de33b
Lalit <lalit@SQMLaptop33.local>
2021-10-20 12:18:38 +0530

    Added templating engie

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const http = require("http");
-const express = require("express");
-const bodyParser = require("express");
-const path = require("path");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
-//const route = require("./route");
-const adminRoute = require("./routes/admin");
-const shopRoute = require("./routes/shop");
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(express.static(path.join(__dirname, 'public'))); // Middleware to serve static file
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/admin', adminRoute);
-app.use(shopRoute);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
-app.listen(3000)
+app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,22 @@
-const express = require("express");
-const path = require("path");
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	console.log("Inside request");
-	res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'))
-	
-})
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
+});
 
 module.exports = router;
